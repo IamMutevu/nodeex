84f230d19efc576ebbeb8bc941fb5374ca90622a
iammutevu <ianmutevu96@gmail.com>
2022-12-20 15:12:04 +0300

    Create a simple web service

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello world")
+// Import module that contains the createServer function 
+var http = require('http')
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end("Welcome Back Alien!!!")
+
+}).listen(3000)
