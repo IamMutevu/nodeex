3bc28a04d13872b1019e42f2da0472fc09a140a7
iammutevu <ianmutevu96@gmail.com>
2023-01-05 16:16:14 +0300

    Exporting multiple functions

--- calc.js
+++ calc.js
@@ -2,4 +2,9 @@ function add(a, b){
     return a+b
 }
 
+function sub(a,b){
+ return a-b
+}
+
 module.exports.add = add
+module.exports.sub = sub
--- modules.js
+++ modules.js
@@ -1,5 +1,7 @@
 var calc = require('./calc.js')
 
 result = calc.add(4,5)
+sub_result = calc.sub(5,2)
 
 console.log("The output is "+ result)
+console.log("The subtract output is "+ sub_result)
