b33868e57c72db0c49798081de861a0ea72d8756
iammutevu <ianmutevu96@gmail.com>
2023-01-05 16:19:02 +0300

    Add second module exportation approach

--- calc.js
+++ calc.js
@@ -6,8 +6,13 @@ function sub(a,b){
  return a-b
 }
 
+
 // module.exports.add = add
 // module.exports.sub = sub
 
 exports.add = add
 exports.sub = sub
+
+exports.multiplication = function(a,b){
+    return a*b
+}
--- modules.js
+++ modules.js
@@ -5,3 +5,4 @@ sub_result = calc.sub(5,2)
 
 console.log("The output is "+ result)
 console.log("The subtract output is "+ sub_result)
+console.log("The multiplication output is "+ calc.multiplication(2,4))
